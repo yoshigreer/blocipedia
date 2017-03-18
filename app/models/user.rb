2e85b52815dba86810678ba3a92567852647fcc6
Trenton Greer <yoshigreer@gmail.com>
2017-03-18 13:15:55 -0600

    User story complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,18 @@ class User < ActiveRecord::Base
   # Only allow letter, number, underscore, and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
-attr_
   attr_accessor :login
 
   def self.find_first_by_auth_conditions(warden_conditions)
-  conditions = warden_conditions.dup
-  if login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  else
-    if conditions[:username].nil?
-      where(conditions).first
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
-      where(username: conditions[:username]).first
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
     end
   end
 
