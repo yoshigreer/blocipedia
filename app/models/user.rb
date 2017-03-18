a7df912b83cd91eb46f009e559efb548154a347d
Trenton Greer <yoshigreer@gmail.com>
2017-03-18 12:51:06 -0600

    finished tutorial, will see if password reset and login work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, 
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
@@ -9,8 +9,22 @@ class User < ActiveRecord::Base
   # Only allow letter, number, underscore, and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
+attr_
   attr_accessor :login
 
+  def self.find_first_by_auth_conditions(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  else
+    if conditions[:username].nil?
+      where(conditions).first
+    else
+      where(username: conditions[:username]).first
+    end
+  end
+
+=begin
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     conditions[:email].downcase! if conditions[:email]
@@ -24,4 +38,5 @@ class User < ActiveRecord::Base
       end
     end
   end
+=end
 end
